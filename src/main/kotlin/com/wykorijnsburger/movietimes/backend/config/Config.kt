b6b48dbf557dd1d4a18b870b53817d3f27e0e820
Wyko Rijnsburger <utwyko@gmail.com>
2017-02-18 10:16:03 +0100

    Don' fail startup on not found property files.

--- src/main/kotlin/com/wykorijnsburger/movietimes/backend/config/Config.kt
+++ src/main/kotlin/com/wykorijnsburger/movietimes/backend/config/Config.kt
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
 
 @Configuration
-@PropertySource("classpath:apikeys.properties", "classpath:security.properties")
+@PropertySource("classpath:apikeys.properties", "classpath:security.properties", ignoreResourceNotFound=true)
 open class Config {
 
 }
