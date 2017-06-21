6330995b0659867aa094967b9f99c8d1d8d57acb
Wyko Rijnsburger <utwyko@gmail.com>
2017-06-21 21:00:14 +0200

    Fix orNull Optional method always returning null.

--- src/main/kotlin/com/wykorijnsburger/movietimes/backend/utils/OptionalUtils.kt
+++ src/main/kotlin/com/wykorijnsburger/movietimes/backend/utils/OptionalUtils.kt
@@ -3,5 +3,9 @@ package com.wykorijnsburger.movietimes.backend.utils
 import java.util.*
 
 fun <T : Any> Optional<T>.orNull(): T? {
+    if (this.isPresent) {
+        return this.get()
+    }
+
     return null
 }
